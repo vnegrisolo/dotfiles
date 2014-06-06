7e2b67d77ca0868d6cf098fc45c1b53f81218bb8
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-06-06 13:24:02 -0300

    reuse the iterate over a folder

--- bin/install.rb
+++ bin/install.rb
@@ -6,32 +6,36 @@ class Install < Thor::Group
   include Thor::Actions
 
   desc 'install the workstation'
+
   def self.source_root
     File.dirname(__FILE__)
   end
 
   def link_dotfiles
-    if yes?('link all dotfiles? [y|n]')
-      Dir.foreach("dotfiles/") do |file|
-        next if file == '.' or file == '..'
-        link_file "../dotfiles/#{file}", "~/.#{file}"
-      end
+    return if no? 'link the dotfiles? [y|n]'
+    files_in("dotfiles/") do |file|
+      link_file "../dotfiles/#{file}", "~/.#{file}"
     end
   end
 
   def link_vim_plugins_conf
-    if yes?('link all vim plugin conf files? [y|n]')
-      empty_directory '~/.vim/plugin'
-      link_file '../vim/plugin/settings', '~/.vim/plugin/settings'
+    return if no? 'link the vim plugin conf files? [y|n]'
+    empty_directory '~/.vim/plugin'
+    link_file '../vim/plugin/settings', '~/.vim/plugin/settings'
+  end
+
+  def link_functions
+    return if no? 'link the function files? [y|n]'
+    files_in("functions/") do |file|
+      link_file "../functions/#{file}", "~/#{file}"
     end
   end
 
-  def link_scripts
-    if yes?('link all function files? [y|n]')
-      Dir.foreach("functions/") do |file|
-        next if file == '.' or file == '..'
-        link_file "../functions/#{file}", "~/#{file}"
-      end
+  private
+
+  def files_in(folder)
+    Dir.foreach(folder) do |file|
+      yield file if file != '.' and file != '..'
     end
   end
 
