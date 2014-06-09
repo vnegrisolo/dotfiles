159e2b78010eebbfe6893b4349364b98fca8f13d
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-06-09 19:03:20 -0300

    put red color to install questions

--- bin/install.rb
+++ bin/install.rb
@@ -12,20 +12,20 @@ class Install < Thor::Group
   end
 
   def link_dotfiles
-    return if no? 'link the dotfiles? [y|n]'
+    return if no? 'link the dotfiles? [y|n]', :red
     files_in("dotfiles/") do |file|
       link_file "../dotfiles/#{file}", "~/.#{file}"
     end
   end
 
   def link_vim_plugins_conf
-    return if no? 'link the vim plugin conf files? [y|n]'
+    return if no? 'link the vim plugin conf files? [y|n]', :red
     empty_directory '~/.vim/plugin'
     link_file '../vim/plugin/settings', '~/.vim/plugin/settings'
   end
 
   def link_functions
-    return if no? 'link the function files? [y|n]'
+    return if no? 'link the function files? [y|n]', :red
     files_in("functions/") do |file|
       link_file "../functions/#{file}", "~/#{file}"
     end
