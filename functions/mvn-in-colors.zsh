15ff5c2b35ae0e8c1ceb7ebdb903d595f34791bf
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-09-19 13:49:07 -0300

    refactor the sed_command in _color functions

--- functions/mvn-in-colors.zsh
+++ functions/mvn-in-colors.zsh
@@ -1,38 +1,34 @@
-source ~/.functions/colors.zsh;
+build_sed_command() {
+  sed_command="sed";
+  sed_replacement $color_white '.*-\{55\}\+$' '.*\[INFO\] Building.*' '^Running .*' '^ T E S T S$' '^Results.*';
+  sed_replacement $color_cyan '.*\[INFO\] Scanning for projects.*';
+  sed_replacement $color_green '^Tests run:.*Failures: 0.*Errors: 0.*Skipped: 0.*' '.*\[INFO\] BUILD SUCCESS$';
+  sed_replacement $color_yellow '.*\[WARNING].*' '.*WARN.*' '^NOTE: Maven is executing in offline mode\.' '^Tests run:.*Failures: 0, Errors: 0, Skipped: [^0].*';
+  sed_replacement $color_red '.*\[ERROR\].*' '.*\[INFO\] BUILD FAILURE' '.* <<< FAILURE!$' '.* <<< ERROR!$' '^Tests in error:.*' '^Tests run:.*Failures: [^0].*' '^Tests run:.*Errors: [^0].*';
+}
 
 mvn-in-colors() {
-  colors;
-
-  temp="/tmp/$0-`date +'%F-%T'`-${RANDOM}"
-  rm -f ${temp};
+  temp="/tmp/$0-`date +'%F-%T'`-${RANDOM}";
   tmp_file=`mktemp ${temp}`;
 
-  (command /opt/maven/bin/mvn $@ ; echo $? > $tmp_file) | sed \
-    -e "s/\(.*-\{55\}\+$\)/${color_white}\1${color_reset}/g" \
-    -e "s/\(.*\[INFO\] Scanning for projects.*\)/${color_cyan}\1${color_reset}/g" \
-    -e "s/\(.*\[INFO\] Building.*\)/${color_white}\1${color_reset}/g" \
-    -e "s/\(^Running .*\)/${color_white}\1${color_reset}/g" \
-    -e "s/\(^ T E S T S$\)/${color_white}\1${color_reset}/g" \
-    -e "s/\(^Results.*\)/${color_white}\1${color_reset}/g" \
-    -e "s/\(^Tests run:.*Failures: 0.*Errors: 0.*Skipped: 0.*\)/${color_green}\1${color_reset}/g" \
-    -e "s/\(.*\[INFO\] BUILD SUCCESS$\)/${color_green}\1${color_reset}/g" \
-    -e "s/\(.*\[WARNING].*\)/${color_yellow}\1${color_reset}/g" \
-    -e "s/\(.*WARN.*\)/${color_yellow}\1${color_reset}/g" \
-    -e "s/\(^NOTE: Maven is executing in offline mode\.\)/${color_yellow}\1${color_reset}/g" \
-    -e "s/\(^Tests run:.*Failures: 0, Errors: 0, Skipped: [^0].*\)/${color_yellow}\1${color_reset}/g" \
-    -e "s/\(.*\[ERROR\].*\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(.*\[INFO\] BUILD FAILURE\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(.* <<< FAILURE!$\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(.* <<< ERROR!$\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(^Tests in error:.*\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(^Tests run:.*Failures: [^0].*\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(^Tests run:.*Errors: [^0].*\)/${color_red}\1${color_reset}/g"
+  build_sed_command;
 
-  echo -ne ${color_reset}
-
+  (command /opt/maven/bin/mvn $@ ; echo $? > $tmp_file) | eval $sed_command;
   read exit_code < $tmp_file
-
   rm -f ${temp};
 
-  return $exit_code
+  return $exit_code;
+}
+
+mvn-debug-in-colors() {
+  temp="/tmp/$0-`date +'%F-%T'`-${RANDOM}";
+  tmp_file=`mktemp ${temp}`;
+
+  build_sed_command;
+
+  (command /opt/maven/bin/mvnDebug $@ ; echo $? > $tmp_file) | eval $sed_command;
+  read exit_code < $tmp_file
+  rm -f ${temp};
+
+  return $exit_code;
 }
--- functions/tail-in-colors.zsh
+++ functions/tail-in-colors.zsh
@@ -1,33 +1,18 @@
-source ~/.functions/colors.zsh;
-
 tail-in-colors() {
-  colors;
-
-  temp="/tmp/$0-`date +'%F-%T'`-${RANDOM}"
-  rm -f ${temp};
+  temp="/tmp/$0-`date +'%F-%T'`-${RANDOM}";
   tmp_file=`mktemp ${temp}`;
 
-  (command tail $@ ; echo $? > $tmp_file) | sed \
-    -e "s/\(DEBUG\)/${color_cyan}\1${color_reset}/g" \
-    -e "s/\(debug\)/${color_cyan}\1${color_reset}/g" \
-    -e "s/\(INFO\)/${color_green}\1${color_reset}/g" \
-    -e "s/\(info\)/${color_green}\1${color_reset}/g" \
-    -e "s/\(WARN\)/${color_yellow}\1${color_reset}/g" \
-    -e "s/\(warn\)/${color_yellow}\1${color_reset}/g" \
-    -e "s/\(FATAL\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(fatal\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(ERROR\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(Error\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(error\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(EXCEPTION\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(Exception\)/${color_red}\1${color_reset}/g" \
-    -e "s/\(exception\)/${color_red}\1${color_reset}/g"
+  sed_command="sed";
+  sed_replacement $color_cyan 'DEBUG' 'debug';
+  sed_replacement $color_green 'INFO' 'info';
+  sed_replacement $color_yellow 'WARN' 'warn';
+  sed_replacement $color_red 'FATAL' 'fatal';
+  sed_replacement $color_red 'ERROR' 'Error' 'error';
+  sed_replacement $color_red 'EXCEPTION' 'Exception' 'exception';
 
-  echo -ne ${color_reset}
-
+  (command tail $@ ; echo $? > $tmp_file) | eval $sed_command;
   read exit_code < $tmp_file
+  rm ${temp};
 
-  rm -f ${temp};
-
-  return $exit_code
+  return $exit_code;
 }
