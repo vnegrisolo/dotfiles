9c8780bc07dc093b8664fd4f3e48a6078bbca940
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-09-23 10:24:37 -0300

    improve the mvm in colors function

--- functions/mvn-in-colors.zsh
+++ functions/mvn-in-colors.zsh
@@ -1,10 +1,15 @@
 build_mvn_sed_command() {
   sed_command="sed";
-  sed_replacement $color_white '.*-\{55\}\+$' '.*\[INFO\] Building.*' '^Running .*' '^ T E S T S$' '^Results.*';
+  sed_replacement $color_white '.*-\{55\}\+$' '.*\[INFO\] Building.*' '^Running .*'
+  sed_replacement $color_white '^ T E S T S$' '^Results.*';
   sed_replacement $color_cyan '.*\[INFO\] Scanning for projects.*';
-  sed_replacement $color_green '^Tests run:.*Failures: 0.*Errors: 0.*Skipped: 0.*' '.*\[INFO\] BUILD SUCCESS$';
-  sed_replacement $color_yellow '.*\[WARNING].*' '.*WARN.*' '^NOTE: Maven is executing in offline mode\.' '^Tests run:.*Failures: 0, Errors: 0, Skipped: [^0].*';
-  sed_replacement $color_red '.*\[ERROR\].*' '.*\[INFO\] BUILD FAILURE' '.* <<< FAILURE!$' '.* <<< ERROR!$' '^Tests in error:.*' '^Tests run:.*Failures: [^0].*' '^Tests run:.*Errors: [^0].*';
+  sed_replacement $color_green '^Tests run:.*Failures: 0.*Errors: 0.*Skipped: 0.*'
+  sed_replacement $color_green '.*\[INFO\] BUILD SUCCESS$';
+  sed_replacement $color_yellow '.*\[WARNING].*' '.*WARN.*'
+  sed_replacement $color_yellow '^NOTE: Maven is executing in offline mode\.'
+  sed_replacement $color_yellow '^Tests run:.*Failures: 0, Errors: 0, Skipped: [^0].*';
+  sed_replacement $color_red '.*\[ERROR\].*' '.* <<< ERROR!$' '.*\[INFO\] BUILD FAILURE' '.* <<< FAILURE!$'
+  sed_replacement $color_red '^Tests in error:.*' '^Tests run:.*Failures: [^0].*' '^Tests run:.*Errors: [^0].*';
 }
 
 mvn-in-colors() {
