c5dbf82938b73fbaaeb4f5b57ee668c99b21272e
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-02-26 15:33:13 -0500

    Add mux as a shell function

---
+++ functions/mux.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+mux() {
+  echo "mux running"
+  project=${1%/};
+
+  if [ -z "${project}" ]; then
+    echo 'usage ./tmux.sh <project_name>';
+  else
+    cd ${project};
+    tmux new -d -s ${project};
+
+    tmux rename-window -t ${project}:0 code;
+    tmux select-window -t ${project}:0;
+    tmux split-window -h 'vim .';
+
+    tmux select-window -t ${project}:0;
+
+    echo "New session on tmux=${project}";
+    cd ..;
+  fi
+
+  echo "Tmux sessions:"
+  tmux ls;
+}
